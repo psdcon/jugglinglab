fcc8eec742c631257cf4f042e973069acc4610f0
romain.richard.IT.engineer <romain.richard.IT.engineer@1dfd3389-90ce-03d6-8155-ddd0afc266ac>
2011-01-23 16:07:50 +0000

    No force close.

--- trunk/src/com/jonglen7/jugglinglab/ui/MyProfileActivity.java
+++ trunk/src/com/jonglen7/jugglinglab/ui/MyProfileActivity.java
@@ -2,15 +2,31 @@ package com.jonglen7.jugglinglab.ui;
 
 import android.app.TabActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TabHost;
 
 import com.jonglen7.jugglinglab.R;
 
 public class MyProfileActivity extends TabActivity {
+	
+	private TabHost tabHost;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+        buildTabs();
     }
+    
+	private void buildTabs(){
+        tabHost = getTabHost();
+
+        tabHost.addTab(tabHost.newTabSpec("tab_test1").setIndicator("Favs").setContent(R.id.my_profile_content));
+        tabHost.addTab(tabHost.newTabSpec("tab_test2").setIndicator("Practicing").setContent(R.id.my_profile_content));
+        tabHost.addTab(tabHost.newTabSpec("tab_test3").setIndicator("Records").setContent(R.id.my_profile_content));
+
+        tabHost.setCurrentTab(0);
+	}
 
 }
