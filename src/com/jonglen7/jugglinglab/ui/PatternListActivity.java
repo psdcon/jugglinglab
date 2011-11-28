a4c1016d7123c05a4029330db9ac9e146f46563f
romain.richard.IT.engineer@gmail.com <romain.richard.IT.engineer@gmail.com@1dfd3389-90ce-03d6-8155-ddd0afc266ac>
2011-11-28 22:00:43 +0000

    Added the 3-Cascade Tricks to the Pattern List.

--- src/com/jonglen7/jugglinglab/ui/PatternListActivity.java
+++ src/com/jonglen7/jugglinglab/ui/PatternListActivity.java
@@ -1,21 +1,90 @@
 package com.jonglen7.jugglinglab.ui;
 
-import greendroid.app.GDActivity;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import greendroid.app.GDListActivity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.jonglen7.jugglinglab.R;
+import com.jonglen7.jugglinglab.jugglinglab.core.PatternRecord;
+import com.jonglen7.jugglinglab.util.DataBaseHelper;
 
-public class PatternListActivity  extends GDActivity {
+public class PatternListActivity extends GDListActivity {
+    
+    /** Pattern list. */
+    ArrayList<PatternRecord> pattern_list;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     	setContentView(R.layout.activity_pattern_list);
+        
+        pattern_list = new ArrayList<PatternRecord>();
+        
+        ArrayList<HashMap<String, String>> listItem = createPatternList();
+        
+        SimpleAdapter mSchedule = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.list_item,
+                new String[] {"list_item_text"}, new int[] {R.id.list_item_text});
+
+        ListView listView = getListView();
+        listView.setOnItemClickListener(itemClickListener);
+        listView.setAdapter(mSchedule);
     }
+
+    // TODO: Just a copy/paste of the methods in TutorialsActivity to see if it works, so it might needs some changes
+    private ArrayList<HashMap<String, String>> createPatternList() {
+    	ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+    	
+		HashMap<String, String> map;
+		
+	 	String query = "SELECT T.PATTERN, H.CODE, B.CODE, P.CODE, T.XML_DISPLAY_LINE_NUMBER, TC.ID_COLLECTION, C.XML_LINE_NUMBER " +
+	 					"FROM Trick T, Hands H, Body B, Prop P, TrickCollection TC, Collection C " +
+	 					"WHERE T.ID_HANDS=H.ID_HANDS " + 
+	 					"AND T.ID_BODY=B.ID_BODY " +
+	 					"AND T.ID_PROP=P.ID_PROP " +
+	 					"AND T.ID_TRICK=TC.ID_TRICK " +
+	 					"AND TC.ID_COLLECTION=C.ID_COLLECTION " +
+	 					"ORDER BY TC.ID_COLLECTION";
+	 	Cursor cursor = DataBaseHelper.execQuery(this, query, null);
+        startManagingCursor(cursor);
+
+    	String[] trick = getResources().getStringArray(R.array.trick);
+	 	
+	 	cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+        	String display = trick[cursor.getInt(cursor.getColumnIndex("XML_DISPLAY_LINE_NUMBER"))];
+        	map = new HashMap<String, String>();
+        	map.put("list_item_text", display);
+        	listItem.add(map);
+        	pattern_list.add(new PatternRecord(display, "", "siteswap", createAnim(cursor)));
+            cursor.moveToNext();
+        }
+
+	 	cursor.close();
+    	
+		return listItem;
+	}
+
+	private String createAnim(Cursor cursor) {
+		String anim = "";
+		anim += "pattern=" + cursor.getString(cursor.getColumnIndex("PATTERN"));
+		anim += ";hands=" + cursor.getString(1);
+		anim += ";body=" + cursor.getString(2);
+		anim += ";prop=" + cursor.getString(3);
+		return anim;
+	}
     
     /** Menu button. */
     @Override
@@ -34,4 +103,15 @@ public class PatternListActivity  extends GDActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+    private OnItemClickListener itemClickListener = new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			Intent i = new Intent(PatternListActivity.this, JMLPatternActivity.class);
+	        i.putExtra("pattern_record", pattern_list.get(position));
+	        startActivity(i);
+		}
+    	
+    };
 }
