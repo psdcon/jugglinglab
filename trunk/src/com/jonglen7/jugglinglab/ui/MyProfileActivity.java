8e554281472bdead6d3fdc0acbc19dffd8a22d13
romain.richard.IT.engineer@gmail.com <romain.richard.IT.engineer@gmail.com@1dfd3389-90ce-03d6-8155-ddd0afc266ac>
2012-03-10 15:04:12 +0000

    Added icons to the tabs in My Profile.

--- trunk/src/com/jonglen7/jugglinglab/ui/MyProfileActivity.java
+++ trunk/src/com/jonglen7/jugglinglab/ui/MyProfileActivity.java
@@ -1,6 +1,7 @@
 package com.jonglen7.jugglinglab.ui;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.TabHost;
 
@@ -26,25 +27,26 @@ public class MyProfileActivity extends BaseTabActivity {
     /** Tabs. */
 	private void buildTabs(){
         tabHost = getTabHost();
+        Resources res = getResources();
 
         tabHost.addTab(tabHost.newTabSpec("tab_starred")
-        		.setIndicator(getString(R.string.my_profile_tab_starred))
+        		.setIndicator(getString(R.string.my_profile_tab_starred), res.getDrawable(R.drawable.gd_action_bar_star))
         		.setContent(new Intent(this, MyProfileTabActivity.class)
         				.putExtra("tab", "starred")
         				.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
         tabHost.addTab(tabHost.newTabSpec("tab_unsorted")
-        		.setIndicator(getString(R.string.my_profile_tab_unsorted))
+        		.setIndicator(getString(R.string.my_profile_tab_unsorted), res.getDrawable(R.drawable.gd_action_bar_help))
         		.setContent(new Intent(this, MyProfileTabActivity.class)
         				.putExtra("tab", "unsorted")
         				.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
         // TODO Romain (Stats): Uncomment when ready (Goals, practicing)
 //        tabHost.addTab(tabHost.newTabSpec("tab_goals")
-//        		.setIndicator(getString(R.string.my_profile_tab_goals))
+//        		.setIndicator(getString(R.string.my_profile_tab_goals), res.getDrawable(R.drawable.gd_action_bar_help))
 //        		.setContent(new Intent(this, MyProfileTabActivity.class)
 //        				.putExtra("tab", "goals")
 //        				.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
 //        tabHost.addTab(tabHost.newTabSpec("tab_training")
-//        		.setIndicator(getString(R.string.my_profile_tab_training))
+//        		.setIndicator(getString(R.string.my_profile_tab_training), res.getDrawable(R.drawable.gd_action_bar_help))
 //        		.setContent(new Intent(this, MyProfileTabActivity.class)
 //        				.putExtra("tab", "training")
 //        				.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
