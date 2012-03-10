c7aa56d77d11168027d2516e8bd95020e5dd3246
romain.richard.IT.engineer@gmail.com <romain.richard.IT.engineer@gmail.com@1dfd3389-90ce-03d6-8155-ddd0afc266ac>
2012-03-10 23:05:37 +0000

    Added a dialog while generating the patterns.

--- trunk/src/com/jonglen7/jugglinglab/ui/GeneratorListActivity.java
+++ trunk/src/com/jonglen7/jugglinglab/ui/GeneratorListActivity.java
@@ -2,9 +2,12 @@ package com.jonglen7.jugglinglab.ui;
 
 import java.util.ArrayList;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,24 +15,18 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.Toast;
 
+import com.jonglen7.jugglinglab.R;
 import com.jonglen7.jugglinglab.jugglinglab.core.PatternRecord;
 import com.jonglen7.jugglinglab.jugglinglab.generator.GeneratorTarget;
 import com.jonglen7.jugglinglab.jugglinglab.generator.siteswapGenerator;
 import com.jonglen7.jugglinglab.jugglinglab.util.JuggleExceptionUser;
-import com.jonglen7.jugglinglab.util.DataBaseHelper;
 import com.jonglen7.jugglinglab.util.ListAdapterTrick;
 import com.jonglen7.jugglinglab.util.Trick;
 
-public class GeneratorListActivity extends BaseListActivity {
-	
-	/** Settings for the generator. */
-	int max_patterns;
-	int max_seconds;
-    
-    /** GeneratorTarget. */
-    GeneratorTarget target;
+public class GeneratorListActivity extends BaseListActivity implements Runnable {
     
     /** Pattern list. */
+	ProgressDialog dialog;
     ArrayList<PatternRecord> pattern_list;
 
     /** ListView. */
@@ -42,65 +39,77 @@ public class GeneratorListActivity extends BaseListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        myDbHelper = DataBaseHelper.init(this);
         
-        /** The ArrayList that will populate the ListView. */
-        pattern_list = createPatternList();
-        
-        setTitle(pattern_list.size() + " patterns found");
-
-        listView = getListView();
-        mSchedule = new ListAdapterTrick(listView, getLayoutInflater(), pattern_list, this, getIntent(), GeneratorListActivity.this);
-        
-        listView.setOnItemClickListener(itemClickListener);
-        listView.setOnItemLongClickListener(itemLongClickListener);
-        listView.setAdapter(mSchedule);
-
-        myDbHelper.close();
+        dialog = ProgressDialog.show(this,
+        		getResources().getString(R.string.please_wait),
+        		getResources().getString(R.string.generator_list_generating_siteswaps),
+        		true);
+        Thread thread = new Thread(this);
+        thread.start();
         
         /** QuickAction. */
         quickActionGrid = new QuickActionGridTrick(this);
     }
 
-    private ArrayList<PatternRecord> createPatternList() {
+    public void run() {
+    	pattern_list = new ArrayList<PatternRecord>();
+    	
         /** Settings for the generator. */
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        max_patterns = Integer.parseInt(preferences.getString("generator_max_patterns", "100"));
-        max_seconds = Integer.parseInt(preferences.getString("generator_max_seconds", "3"));
+        int max_patterns = Integer.parseInt(preferences.getString("generator_max_patterns", "100"));
+        int max_seconds = Integer.parseInt(preferences.getString("generator_max_seconds", "3"));
         
         /** GeneratorTarget. */
-        target = new GeneratorTarget();
+        GeneratorTarget target = new GeneratorTarget();
         target.clearPatternList();
         
         /** Get pattern. */
+        String pattern = null;
         Bundle extras = getIntent().getExtras();
-        if (extras== null){
-        	Toast.makeText(getApplicationContext(), "ERROR",
-                    Toast.LENGTH_SHORT).show();
-        }
-        String pattern = extras.getString("pattern");
-        
-        /** Generate siteswap. */
-        siteswapGenerator sg = new siteswapGenerator();
         try {
-			sg.initGenerator(pattern);
-			sg.runGenerator(target, max_patterns, max_seconds);
-		} catch (JuggleExceptionUser e) {
-			e.printStackTrace();
-		}
+        	pattern = extras.getString("pattern");
+        } catch (Exception e) {
+        	Toast.makeText(getApplicationContext(), getResources().getString(R.string.error_pattern), Toast.LENGTH_SHORT).show();
+        }
+            
+        if (pattern != null) {
+            /** Generate siteswap. */
+            siteswapGenerator sg = new siteswapGenerator();
+            try {
+    			sg.initGenerator(pattern);
+    			sg.runGenerator(target, max_patterns, max_seconds);
+    		} catch (JuggleExceptionUser e) {
+    			e.printStackTrace();
+    		}
 
-        ArrayList<PatternRecord> pattern_list = target.getPattern_list();
-        
-        /** Get the names of the patterns (if there is one) */
-        for (PatternRecord pattern_record : pattern_list) {
-        	String display = new Trick(pattern_record, this).getCUSTOM_DISPLAY();
-        	if (display != "") pattern_record.setDisplay(display);
-        	pattern_record.setDisplay(pattern_record.getDisplay().trim());
+            pattern_list = target.getPattern_list();
+            
+            /** Get the names of the patterns (if there is one) */
+            for (PatternRecord pattern_record : pattern_list) {
+            	String display = new Trick(pattern_record, this).getCUSTOM_DISPLAY();
+            	if (display != "") pattern_record.setDisplay(display);
+            	pattern_record.setDisplay(pattern_record.getDisplay().trim());
+            }
         }
         
-        return pattern_list;
+        handler.sendEmptyMessage(0);
     }
+    
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            setTitle(getResources().getString(R.string.generator_list_title, pattern_list.size()));
+
+            listView = getListView();
+            mSchedule = new ListAdapterTrick(listView, getLayoutInflater(), pattern_list, GeneratorListActivity.this, getIntent(), GeneratorListActivity.this);
+            
+            listView.setOnItemClickListener(itemClickListener);
+            listView.setOnItemLongClickListener(itemLongClickListener);
+            listView.setAdapter(mSchedule);
+
+        	dialog.dismiss();
+        }
+    };
     
     private OnItemClickListener itemClickListener = new OnItemClickListener() {
 
