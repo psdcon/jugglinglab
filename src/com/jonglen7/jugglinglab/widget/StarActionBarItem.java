7beb08fe9869e1ef074b73504fe555020cf8f298
romain.richard.IT.engineer@gmail.com <romain.richard.IT.engineer@gmail.com@1dfd3389-90ce-03d6-8155-ddd0afc266ac>
2012-03-12 05:24:12 +0000

    Added comments to StarActionBarItem.

--- src/com/jonglen7/jugglinglab/widget/StarActionBarItem.java
+++ src/com/jonglen7/jugglinglab/widget/StarActionBarItem.java
@@ -9,40 +9,49 @@ import com.cyrilmottier.android.greendroid.R;
 import com.jonglen7.jugglinglab.util.Collection;
 import com.jonglen7.jugglinglab.util.Trick;
 
+/**
+ * An extension of a {@link NormalActionBarItem} that supports a starred states.
+ * 
+ * @author Romain Richard
+ */
 public class StarActionBarItem extends NormalActionBarItem {
 
     private CheckBox mCheckBox;
-    private Trick trick;
+    private Trick mTrick;
 
-    public StarActionBarItem() {
+    public StarActionBarItem() {}
+
+    @Override
+    protected View createItemView() {
+        return LayoutInflater.from(mContext).inflate(R.layout.action_bar_item_star, mActionBar, false);
     }
-    
-    public void setTrick(Trick trick) {
-    	super.getItemView();  // To prevent NullPointerException
-    	this.trick = trick;
-    	boolean isStarred = false;
-    	for (Collection collection : trick.getCollections()) {
-    		if (collection.isStarred()) isStarred = true;
-    	}
-    	this.mCheckBox.setChecked(isStarred);
-	}
 
     @Override
     protected void prepareItemView() {
         super.prepareItemView();
         mCheckBox = (CheckBox) mItemView.findViewById(R.id.btn_star);
     }
-
-    @Override
-    protected View createItemView() {
-        return LayoutInflater.from(mContext).inflate(R.layout.action_bar_item_star, mActionBar, false);
-    }
     
 	@Override
     protected void onItemClicked() {
         super.onItemClicked();
     	mCheckBox.setChecked(!mCheckBox.isChecked());
-    	trick.star();
+    	mTrick.star();
     }
+    
+    /**
+     * Sets the starring state of this {@link StarActionBarItem}.
+     * 
+     * @param trick The Trick to (un)star.
+     */
+    public void setTrick(Trick trick) {
+    	super.getItemView();  // To prevent NullPointerException
+    	this.mTrick = trick;
+    	boolean isStarred = false;
+    	for (Collection collection : trick.getCollections()) {
+    		if (collection.isStarred()) isStarred = true;
+    	}
+    	this.mCheckBox.setChecked(isStarred);
+	}
 	
 }
