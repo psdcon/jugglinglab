4398d57b709f5dfc8555a7deba2672d9119e1ed2
frederic.rayar <frederic.rayar@1dfd3389-90ce-03d6-8155-ddd0afc266ac>
2012-10-11 17:23:52 +0000

    Add Axis Class (Empty right now)

---
+++ trunk/src/com/jonglen7/jugglinglab/jugglinglab/renderer/Axis.java
@@ -0,0 +1,33 @@
+package com.jonglen7.jugglinglab.jugglinglab.renderer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+
+/**
+ * 
+ * Class Axis
+ * Build a 3D axis
+ * 
+ * @author frédéric.rayar
+ * @date 11/10/2012
+ *
+ */
+
+public class Axis {
+	
+	// Default Constructor
+	public Axis()
+	{
+		
+	}
+	
+	
+	
+	// Draw Method
+	public void draw(GL10 gl)
+	{
+
+
+	}
+
+}
