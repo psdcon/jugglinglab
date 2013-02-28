b850bdca0f388a06d161ff5092ac2630682b6104
frederic.rayar <frederic.rayar@1dfd3389-90ce-03d6-8155-ddd0afc266ac>
2013-02-28 15:41:02 +0000

    AboutActivity updated

--- trunk/src/com/jonglen7/jugglinglab/ui/AboutActivity.java
+++ trunk/src/com/jonglen7/jugglinglab/ui/AboutActivity.java
@@ -1,6 +1,10 @@
 package com.jonglen7.jugglinglab.ui;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.jonglen7.jugglinglab.R;
 
@@ -12,5 +16,19 @@ public class AboutActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setActionBarContentView(R.layout.activity_about);
+        
+        
+        
+        /// Version      
+        TextView  txt_version = (TextView) findViewById(R.id.about_version);
+        PackageManager manager = this.getPackageManager();
+        PackageInfo info;
+		try {
+			info = manager.getPackageInfo(this.getPackageName(), 0);
+			 txt_version.setText("Version " + info.versionName);
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+       
     }
 }
