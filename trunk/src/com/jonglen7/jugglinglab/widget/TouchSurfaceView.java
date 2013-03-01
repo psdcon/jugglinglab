a153a6e0122a6fa4dadfb85fab11c7446cb6b7a0
frederic.rayar <frederic.rayar@1dfd3389-90ce-03d6-8155-ddd0afc266ac>
2013-03-01 16:14:23 +0000

    Zoom issue solved (min api level is now 8)

--- trunk/src/com/jonglen7/jugglinglab/widget/TouchSurfaceView.java
+++ trunk/src/com/jonglen7/jugglinglab/widget/TouchSurfaceView.java
@@ -4,140 +4,133 @@ import android.content.Context;
 import android.graphics.PointF;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
-import android.util.FloatMath;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 
 import com.jonglen7.jugglinglab.jugglinglab.renderer.JugglingRenderer;
 
 /**
- * Implement a simple rotation control in the X-Y plan
- * Pinch-to-zoom found here: http://media.pragprog.com/titles/eband3/code/Touchv1/src/org/example/touch/Touch.java
+ * 
+ * Implement a touch surface
+ * Simple rotation around axes x and y
+ * Pinch-to-zoom with regard to http://android-developers.blogspot.fr/2010/06/making-sense-of-multitouch.html
+ * 
  */
 public class TouchSurfaceView extends GLSurfaceView {
 
-//    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;  // TODO Fred|Romain: Too fast on actual devices ?
+	/** Attributes **/
     private final float TOUCH_SCALE_FACTOR = 180.0f / 640;
     private final float TRACKBALL_SCALE_FACTOR = 36.0f;
+   
     private JugglingRenderer mRenderer;
 	private PointF mPrevious = new PointF();
-    private PointF mPreviousMid = new PointF();
-    private PointF mNewMid = new PointF();
-	
-	boolean isOnPause = false;
-	float move = 0;
-	float oldDist = 1f;
-	static final float distEpsilon = 0.1f;
+	private boolean isOnPause = false;
+    private ScaleGestureDetector mScaleDetector;
+    boolean motion = false;
 
-    // We can be in one of these 3 states
-    static final int NONE = 0;
-    static final int ROTATE = 1;
-    static final int ZOOM = 2;
-    int mode = NONE;
 	
-    public TouchSurfaceView(Context context) {
+    /** Constructor **/
+    public TouchSurfaceView(Context context) 
+    {
         super(context);
-        init();
+        init(context);
     }
 
-    public TouchSurfaceView(Context context, AttributeSet attrs) {
+    /** Constructor **/
+    public TouchSurfaceView(Context context, AttributeSet attrs) 
+    {
         super(context, attrs);
-        init();
+        init(context);
     }
     
-    private void init() {
+    /** Initialization method **/
+    private void init(Context context) 
+    {
+    	// Set focus
     	setFocusable(true);
+    	
+    	// Create our ScaleGestureDetector
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
     }
+    
     
     @Override
-    public void setRenderer(Renderer renderer) {
+    public void setRenderer(Renderer renderer) 
+    {
     	mRenderer = (JugglingRenderer) renderer;
     	mRenderer.mZoom = 1;
     	super.setRenderer(mRenderer);
     }
     
-    @Override public boolean onTrackballEvent(MotionEvent e) {
+    @Override 
+    public boolean onTrackballEvent(MotionEvent e) 
+    {
         mRenderer.mAngleX += e.getX() * TRACKBALL_SCALE_FACTOR;
         mRenderer.mAngleY += e.getY() * TRACKBALL_SCALE_FACTOR;
         requestRender();
         return true;
     }
 
-    @Override public boolean onTouchEvent(MotionEvent e) {
+    @Override 
+    public boolean onTouchEvent(MotionEvent e) 
+    {
+    	// Let the ScaleGestureDetector inspect all events.
+        mScaleDetector.onTouchEvent(e);
+
+        // Get current touch coordinates
         float x = e.getX();
         float y = e.getY();
-        switch (e.getAction() & MotionEvent.ACTION_MASK) {
-        case MotionEvent.ACTION_DOWN:
-            mode = ROTATE;
-            break;
-         case MotionEvent.ACTION_POINTER_DOWN:
-            oldDist = spacing(e);
-            if (oldDist > 10f) {
-               mode = ZOOM;
-               midPoint(mPreviousMid, e);
-            }
-            break;
-        case MotionEvent.ACTION_MOVE:
-        	if (!isOnPause) {
-        		float dx;
-        		float dy;
-        		if (mode == ROTATE) {
-                    dx = x - mPrevious.x;
-                    dy = y - mPrevious.y;
-                    mRenderer.mAngleX += dx * TOUCH_SCALE_FACTOR;
-                    mRenderer.mAngleY += dy * TOUCH_SCALE_FACTOR;
-                    requestRender();
-                    move += Math.abs(dx) + Math.abs(dy);
-        		} else if (mode == ZOOM) {											
-        			float newDist = spacing(e);
-        			if (Math.abs((newDist - oldDist)/oldDist) < distEpsilon) {
-        				midPoint(mNewMid, e);
-        				dx = mNewMid.x - mPreviousMid.x;
-        				dy = mNewMid.y - mPreviousMid.y;
-        				mRenderer.mTranslateX += dx * TOUCH_SCALE_FACTOR;
-        				mRenderer.mTranslateY -= dy * TOUCH_SCALE_FACTOR;
-        				requestRender();
-        				mPreviousMid.set(mNewMid);
-        			} else if (newDist > 10f) {
-        				zoom(newDist / oldDist);							// TODO FRED: Correct TouchZoom (cf usbkey.JLD2.txt)
-        				mRenderer.mZoom = newDist / oldDist;				//           Issue here : double and inconsistent modification of mRenderer.mZoom ?!
-        				requestRender();
-        			}
+        
+        // Switch detection action : either move (rotate, zoom) or pause/resume
+        switch (e.getAction() & MotionEvent.ACTION_MASK) 
+        {
+        	// Move action (rotate or zoom)
+	        case MotionEvent.ACTION_MOVE:
+	        	
+	        	// Only move if animation is not paused
+	        	if (!isOnPause) 
+	        	{
+	        		float dx;
+	        		float dy;
+	        		
+	        		// Only ROTATE if the ScaleGestureDetector isn't processing a gesture.
+	        		if (!mScaleDetector.isInProgress()) 
+	        		{
+	                    dx = x - mPrevious.x;
+	                    dy = y - mPrevious.y;
+	                    mRenderer.mAngleX += dx * TOUCH_SCALE_FACTOR;
+	                    mRenderer.mAngleY += dy * TOUCH_SCALE_FACTOR;
+	                    requestRender();
+	        		} 
+	        		
+	        		motion = true;
+	        	}
+	            break;
+	            
+	        // Pause/Resume when first finger lifted
+	        case MotionEvent.ACTION_UP:
+        		// Only if no gesture.
+        		if (!motion) 
+        		{
+	        		if (isOnPause)
+	            		onResume();
+	            	else
+	            		onPause();
+	        		isOnPause = !isOnPause;
         		}
-        	}
-            break;
-        case MotionEvent.ACTION_UP:
-        case MotionEvent.ACTION_POINTER_UP:
-        	if (mode == ROTATE && move == 0) {
-        		if (isOnPause)
-            		onResume();
-            	else
-            		onPause();
-        		isOnPause = !isOnPause;
-        	}
-        	move = 0;
-        	mode = NONE;
-        	break;
+        		motion = false;
+
+	        	break;
         }
+        
         mPrevious.set(x, y);
         return true;
     }
     
-    private float spacing(MotionEvent event) {
-	   float x = event.getX(0) - event.getX(1);
-	   float y = event.getY(0) - event.getY(1);
-	   return FloatMath.sqrt(x * x + y * y);
-	}
-
-    /** Calculate the mid point of the first two fingers */
-    private void midPoint(PointF point, MotionEvent event) {
-       float x = event.getX(0) + event.getX(1);
-       float y = event.getY(0) + event.getY(1);
-       point.set(x / 2, y / 2);
-    }
-    
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    public boolean onKeyDown(int keyCode, KeyEvent event) 
+    {
             if(keyCode==KeyEvent.KEYCODE_DPAD_UP)
             	zoomIn();
             if(keyCode==KeyEvent.KEYCODE_DPAD_DOWN)
@@ -146,16 +139,30 @@ public class TouchSurfaceView extends GLSurfaceView {
             return true;
     }
     
-    public void zoom(float value) {
-    	mRenderer.mZoom += value;
-    }
-    
+    /** Zoom In **/
     public void zoomIn() {
-    	zoom(.1f);
+    	mRenderer.SetZoomValue(mRenderer.mZoom + .1f);
     }
     
+    /** Zoom Out **/
     public void zoomOut() {
-    	zoom(-.1f);
+    	mRenderer.SetZoomValue(mRenderer.mZoom - .1f);
+    }
+    
+    /** ScaleListener for Pinch To Zoom **/
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+        	
+        	// Update scale factor
+        	mRenderer.SetZoomValue(mRenderer.mZoom*detector.getScaleFactor());
+        	requestRender();
+            
+            // Don't let the object get too small or too large.
+            //mRenderer.mZoom = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
+
+            return true;
+        }
     }
 }
 
